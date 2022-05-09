8b5433644ecc2778a8e7143015767a9872eb3004
jackal911 <thisisdiss@gmail.com>
2022-05-09 14:14:19 +0900

    파일 정리, readme 추가, 강의노트 추가

--- python_nadocoding/9-10_starcraft.py
+++ python_nadocoding/9-10_starcraft.py
@@ -1,3 +1,11 @@
+'''
+'나도코딩'의 파이썬 코딩 무료 강의(기본편) https://www.youtube.com/watch?v=kWiCuklohdY&t=9695s
+9-10 스타크래프트 프로젝트 소스
+'''
+
+from random import *
+
+# 일반 유닛
 class Unit:
     def __init__(self, name, hp, speed):
         self.name = name
@@ -6,7 +14,6 @@ class Unit:
         print("{} 유닛이 생성되었습니다.".format(name))
         
     def move(self, location):
-        print("[지상 유닛 이동]")
         print("{} : {} 방향으로 이동합니다. [속도 {}]".format(self.name, location, self.speed))
         
     def damaged(self, damage):
@@ -77,10 +84,78 @@ class FlyableAttackUnit(AttackUnit, Flyable):
         Flyable.__init__(self, flying_speed)
         
     def move(self, location):
-        print("[공중 유닛 이동]")
         self.fly(self.name, location)
-    
+
+# 레이스    
 class Wraith(FlyableAttackUnit):
     def __init__(self):
         super().__init__("레이스", 80, 20, 5)
         self.clocked = False # 클로킹 모드 (해제 상태)
+    
+    def clocking(self):
+        if self.clocked == True:
+            print("{0} : 클로킹 모드 해제합니다.".format(self.name))
+            self.clocked = False
+        else:
+            print("{0} : 클로킹 모드 설정합니다.".format(self.name))
+            self.clocked = True
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+    
+def game_over():
+    print("Player : GG") # good game
+    print("[Player] 님이 게임에서 퇴장하셨습니다.")
+    
+
+# 게임 진행
+game_start()
+
+# 마린 3기 생성
+m1 = Marine()
+m2 = Marine()
+m3 = Marine()
+
+# 탱크 2기 생성
+t1 = Tank()
+t2 = Tank()
+
+# 레이스 1기 생성
+w1 = Wraith()
+
+# 유닛 일괄 관리
+attack_units = []
+attack_units.append(m1)
+attack_units.append(m2)
+attack_units.append(m3)
+attack_units.append(t1)
+attack_units.append(t2)
+attack_units.append(w1)
+
+# 전군 이동
+for unit in attack_units:
+    unit.move("1시")
+
+# 탱크 시즈모드 개발
+Tank.seize_developed = True
+print("[알림] 탱크 시즈 모드 개발이 완료되었습니다.")
+
+# 공격 모드 준비 (마린 : 스팀팩, 탱크 : 시즈모드, 레이스 : 클로킹)
+for unit in attack_units:
+    if isinstance(unit, Marine):
+        unit.stimpack()
+    elif isinstance(unit, Tank):
+        unit.set_seize_mode()
+    elif isinstance(unit, Wraith):
+        unit.clocking()
+        
+# 전군 공격
+for unit in attack_units:
+    unit.attack("1시")
+
+# 전군 피해
+for unit in attack_units:
+    unit.damaged(randint(5, 21)) # 공격은 랜덤으로 받음 (5 ~ 20)
+    
+# 게임 종료
+game_over()
